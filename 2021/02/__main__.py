1fdac09d1463b78a4dbd55a127631526bc21db4a
odaniels <oskardaniels.90@gmail.com>
2021-12-02 21:11:57 +0100

    2021 02 with recursion

--- 2021/02/__main__.py
+++ 2021/02/__main__.py
@@ -1,24 +1,28 @@
 from pathlib import Path
+import sys
+
+sys.setrecursionlimit(2000)
 
 
 curr_dir = Path(__file__).parent
 with (curr_dir / "input.txt").open() as input:
-    values = [in_line.split(" ") for in_line in input]
-    x, z, aim = 0, 0, 0
-    for command in values:
-        direction = command[0]
-        steps = int(command[1])
-        if direction == "forward":
-            x += steps
-            z += steps * aim # only for PART2
-        if direction == "up":
-            # PART1: z -= steps
-            aim -= steps
-        if direction == "down":
-            # PART1: z += steps
-            aim += steps
 
-    result = x * z
+    def get_position(values):
+        if not values:
+            return 0, 0, 0
+        direction, steps = values[-1].split(" ")
+        x, z, aim = get_position(values[:-1])
+        if direction == "forward":
+            # return (x + int(steps)), z, aim
+            return (x + int(steps)), (z + int(steps) * aim), aim
+        if direction == "up":
+            # return x, (z - int(steps)), aim
+            return x, z, (aim - int(steps))
+        if direction == "down":
+            # return x, (z + int(steps)), aim
+            return x, z, (aim + int(steps))
+    x, z, aim = get_position(input.readlines())
+
     # print(f"PART1: {result}")
-    print(f"PART2: {result}")
+    print(f"PART2: {x * z}")
 
