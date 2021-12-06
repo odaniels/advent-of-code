305e5a6e849f79b250d03caa3e3254135a802c40
odaniels <oskardaniels.90@gmail.com>
2021-12-06 22:18:25 +0100

    Update init script

--- 2021/init.py
+++ 2021/init.py
@@ -1,8 +1,12 @@
 from datetime import date
 from pathlib import Path
 import shutil
+import sys
 
-dir_name = str(date.today())[-2:]
+try:
+    dir_name = sys.argv[1]
+except IndexError:
+    dir_name = str(date.today())[-2:]
 
 curr_dir = Path(__file__).parent
 new_dir = curr_dir / dir_name
