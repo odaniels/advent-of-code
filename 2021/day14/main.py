7899b0f672460295ccaf71d09b3d471648f9e4d3
odaniels <oskardaniels.90@gmail.com>
2021-12-14 19:31:43 +0100

    Some 2021 14 cleanup

--- 2021/day14/main.py
+++ 2021/day14/main.py
@@ -1,41 +1,26 @@
-from collections import defaultdict
+from collections import Counter
 
 
 def main(input):
     start, insertions = input.read().split("\n\n")
-    start = start.strip()
+    pairs = Counter((a,b) for a, b in zip(start.strip()[:-1], start.strip()[1:]))
+    rules = {(line[0], line[1]): line[-1] for line in insertions.split("\n")}
 
-    pairs = defaultdict(int)
-    for a, b in zip(start[:-1], start[1:]):
-        pairs[(a,b)] += 1
-
-    insertion_rules = dict()
-    for line in insertions.split("\n"):
-        pair, insertion = line.split(" -> ")
-        a, b = pair
-        insertion_rules[(a, b)] = insertion
-
-    pairs_steps = [pairs]
-    for _ in range(40):
-        new_pairs = defaultdict(int)
-        for pair, count in pairs.items():
-            if pair in insertion_rules:
-                new_pair1 = (pair[0], insertion_rules[pair])
-                new_pairs[new_pair1] += count
-                new_pair2 = (insertion_rules[pair], pair[1])
-                new_pairs[new_pair2] += count
-        pairs = new_pairs
-        pairs_steps.append(new_pairs)
+    pair_steps = [pairs]
+    for i in range(40):
+        new_pairs = Counter()
+        for pair, count in pair_steps[i].items():
+            if pair in rules:
+                new_pairs[(pair[0], rules[pair])] += count
+                new_pairs[(rules[pair], pair[1])] += count
+        pair_steps.append(new_pairs)
 
     results = []
-    for step in pairs_steps:
-        letters = defaultdict(int)
-        letters[start[-1]] += 1
-        for pair, c in step.items():
-            first, last = pair
-            letters[first] += c
-        letter_counts = sorted(letters.values())
-        results.append(letter_counts[-1] - letter_counts[0])
+    for step in pair_steps:
+        letters = Counter([start[0]])
+        for pair, count in step.items():
+            letters[pair[1]] += count
+        results.append(letters.most_common()[0][1] - letters.most_common()[-1][1])
 
     return results[10], results[40]
 
